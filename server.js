89b8e8489b323e6b7436acd84f303d76e7a2dc2b
soru <ityouna007@gmail.com>
2025-05-17 00:17:22 +0900

    初めて自分で作ったWEBサーバー

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hello server");
+const http = require("http");
+const PORT = 8000;
+
+const server = http.createServer((req, res) =>{
+  res.writeHead(200, { "Content-type": "text/html"});
+  res.write("<h1>Hello world soru</h1>");
+  res.end();
+});
+
+server.listen(PORT,() =>{
+  console.log("server running!");
+})
+
