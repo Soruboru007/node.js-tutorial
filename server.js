434f866c286bd0921f69532f2cbb4fe26f8817df
soru <ityouna007@gmail.com>
2025-05-17 00:28:58 +0900

    サーバー関数内に直接書かずに、他のファイルから読み込むためのコード追加

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const PORT = 8000;
+//htmlファイルを直接読み込ませたい時に使うコード
+const html = require("fs").readFileSync("./index.html");
 
 //webサーバー作ろう
 const server = http.createServer((req, res) =>{
   //ブラウザからアクセスがきた時に処理
   res.writeHead(200, { "Content-type": "text/html"});
-  res.write("<h1>Hello world soru</h1>");
+  res.write(html);
   res.end();
 });
 
